95eddaf9db5ecb1bb72202aec3abc2c5bdfbb452
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-16 22:32:27 +0200

    [frontend] add initial component files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className='flex w-[100vw] h-[100vh] items-center justify-center'>
+      <p className='text-white'>LiveLonger landing page</p>
+    </main>
+  )
+}
+
+export default App
