7254df4cebcb9ad815be92a29bdc57f2b4760af9
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-18 17:16:59 +0200

    [backend] adjust quickMeal from single to multiple in daily meal type

--- backend/src/routes/meals/types/meals_apis.ts
+++ backend/src/routes/meals/types/meals_apis.ts
@@ -1,29 +1,31 @@
-import { MealFull, MealType } from "../../../database/schema/meal";
-import { Nutrients } from "../../../database/schema/product";
-import { QuickMealFull } from "../../../database/schema/quick_meal";
+import {MealFull, MealType} from '../../../database/schema/meal';
+import {Nutrients} from '../../../database/schema/product';
+import {
+  QuickMealFull,
+  QuickMealInfo,
+} from '../../../database/schema/quick_meal';
 
 export type DailyMealsType = {
-    meals: {
-        [type in MealType]?: DailyMealType | DailyQuickMealType;
-    }
+  meals: {
+    [type in MealType]?: DailyMealType | DailyQuickMealType;
+  };
 } & AggregatedNutrientsType;
 
 export type AggregatedNutrientsType = {
-    totalNutrientsGrams: Nutrients | null;
-    totalCalories: number;
-}
+  totalNutrientsGrams: Nutrients | null;
+  totalCalories: number;
+};
 
 export type DailyQuickMealType = {
-    calories: number;
-    quantity: number;
-    quickMeal: QuickMealFull;
-    nutrients_grams: Nutrients;
-} & Pick<MealFull, 'type' | '_id'>
+  calories: number;
+  quantity: number;
+  quickMeals: QuickMealInfo[];
+  nutrients_grams: Nutrients;
+} & Pick<MealFull, 'type'>;
 
 export type DailyMealType = {
-    calories: number;
-    grams: number;
-    nutrients_grams: Nutrients;
-    nutrients_percentage: Nutrients;
-} & Pick<MealFull, 'products' | 'type'>
-
+  calories: number;
+  grams: number;
+  nutrients_grams: Nutrients;
+  nutrients_percentage: Nutrients;
+} & Pick<MealFull, 'products' | 'type'>;
