10a42d153d16d835a3e540f06badda6d6057b5e7
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-18 17:15:45 +0200

    [backend] handle quantity factor for quick meal saving

--- backend/src/network/nutrition.ts
+++ backend/src/network/nutrition.ts
@@ -1,26 +1,36 @@
-import { Meal, MealFull, MealModel } from "../database/schema/meal";
-import { QuickMealModel } from "../database/schema/quick_meal";
-import { getProductDBMeal } from "../libs/product";
-
+import {Meal, MealFull, MealModel} from '../database/schema/meal';
+import {QuickMealModel} from '../database/schema/quick_meal';
+import {getProductDBMeal} from '../libs/product';
+import {QuickMealQuantityFactor} from '../routes/foods/types/product_apis';
 
 export const saveNutrition = async (meal: MealFull): Promise<Meal | null> => {
-    const doc = await MealModel.create(await getProductDBMeal(meal));
-    return doc.toObject();
-}
+  const doc = await MealModel.create(await getProductDBMeal(meal));
+  return doc.toObject();
+};
 
-export const saveQuickMealNutrition = async (meal: Pick<MealFull, 'quickMealId' | 'uid' | 'type'>): Promise<Meal | null> => {
-    const quickMealDoc = await QuickMealModel.findOne({_id: meal.quickMealId, uid: meal.uid}).exec();
-    if(quickMealDoc){
-        const mealData: Meal = {
-            date: new Date(),
-            products: quickMealDoc.products,
-            type: meal.type ?? quickMealDoc.type,
-            uid: meal.uid,
-            quickMealId: meal.quickMealId,
-        }
-        const doc = await MealModel.create(mealData);
-        return doc.toObject();
-    }else{
-        return null;
-    }
-}
+export const saveQuickMealNutrition = async (
+  meal: Pick<MealFull, 'quickMealId' | 'uid' | 'type'>,
+  quantity_factor?: QuickMealQuantityFactor['quantity_factor'],
+): Promise<Meal | null> => {
+  const quickMealDoc = await QuickMealModel.findOne({
+    _id: meal.quickMealId,
+    uid: meal.uid,
+  }).exec();
+  const quantityFactor = quantity_factor ?? 1;
+  if (quickMealDoc) {
+    const mealData: Meal = {
+      date: new Date(),
+      products: quickMealDoc.products.map(p => ({
+        ...p,
+        quantity: p.quantity * quantityFactor,
+      })),
+      type: meal.type ?? quickMealDoc.type,
+      uid: meal.uid,
+      quickMealId: meal.quickMealId,
+    };
+    const doc = await MealModel.create(mealData);
+    return doc.toObject();
+  } else {
+    return null;
+  }
+};
