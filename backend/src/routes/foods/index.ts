03914b1af8b997fd313b34afac728ad00ed8a2ca
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-18 17:15:17 +0200

    [backend] add quantity factor param for quick meals

--- backend/src/routes/foods/index.ts
+++ backend/src/routes/foods/index.ts
@@ -1,68 +1,110 @@
-import express, { NextFunction } from "express";
-import { ErrorMiddlewareType, ErrorMiddlewareTypeProps, TRequest, TResponse } from "../../types";
-import { getProductQRCode, getProductsQueryFood } from "../../network/product";
-import { Product } from "../../database/schema/product";
-import { ErrorMiddlewareTypes, ErrorNames } from "../../constants/ErrorHandling";
-import { ProductQRCodeRequest, ProductQueryRequest, ProductQueryResponse } from "./types/product_apis";
-import { ROUTES } from "../../constants/Routes";
-import { Meal, MealFull } from "../../database/schema/meal";
-import { saveNutrition, saveQuickMealNutrition } from "../../network/nutrition";
+import express, {NextFunction} from 'express';
+import {
+  ErrorMiddlewareType,
+  ErrorMiddlewareTypeProps,
+  TRequest,
+  TResponse,
+} from '../../types';
+import {getProductQRCode, getProductsQueryFood} from '../../network/product';
+import {Product} from '../../database/schema/product';
+import {ErrorMiddlewareTypes, ErrorNames} from '../../constants/ErrorHandling';
+import {
+  ProductQRCodeRequest,
+  ProductQueryRequest,
+  ProductQueryResponse,
+  QuickMealQuantityFactor,
+} from './types/product_apis';
+import {ROUTES} from '../../constants/Routes';
+import {Meal, MealFull} from '../../database/schema/meal';
+import {saveNutrition, saveQuickMealNutrition} from '../../network/nutrition';
 
 export const router = express.Router();
 
-/* 
-    * /foods
-*/
+/*
+ * /foods
+ */
 
-router.get(ROUTES.FOODS.FOODS_SEARCH, async(req: TRequest<any,ProductQueryRequest>, res: TResponse<ProductQueryResponse | ErrorMiddlewareTypeProps>, next) => {
+router.get(
+  ROUTES.FOODS.FOODS_SEARCH,
+  async (
+    req: TRequest<any, ProductQueryRequest>,
+    res: TResponse<ProductQueryResponse | ErrorMiddlewareTypeProps>,
+    next,
+  ) => {
     const {query} = req.query;
     const product = await getProductsQueryFood(query);
-    if(!product) {
-        res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
-        return;
-    }else{
-        res.json(product);
+    if (!product) {
+      res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
+      return;
+    } else {
+      res.json(product);
     }
-})
+  },
+);
 
-
-router.get(ROUTES.FOODS.FOODS_QR_CODE, async(req: TRequest<any,any, ProductQRCodeRequest>, res: TResponse<Product | ErrorMiddlewareTypeProps>) => {
+router.get(
+  ROUTES.FOODS.FOODS_QR_CODE,
+  async (
+    req: TRequest<any, any, ProductQRCodeRequest>,
+    res: TResponse<Product | ErrorMiddlewareTypeProps>,
+  ) => {
     const {qr_code} = req.params;
     const product = await getProductQRCode(qr_code);
-    if(!product) {
-        res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
-        return;
+    if (!product) {
+      res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
+      return;
     }
     res.json(product);
-})
+  },
+);
 
-router.post(ROUTES.FOODS.FOODS_SAVE, async(req: TRequest<MealFull>, res: TResponse<Meal | ErrorMiddlewareTypeProps>) => {
+router.post(
+  ROUTES.FOODS.FOODS_SAVE,
+  async (
+    req: TRequest<MealFull>,
+    res: TResponse<Meal | ErrorMiddlewareTypeProps>,
+  ) => {
     const meal = req.body;
     const {uid} = res.locals.user!;
-    if(!meal || !meal.products) {
-        res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
-        return;
+    if (!meal || !meal.products) {
+      res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
+      return;
     }
     const savedMeal = await saveNutrition({...meal, uid});
-    if(!savedMeal) {
-        res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
-        return;
+    if (!savedMeal) {
+      res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
+      return;
     }
     res.status(200).json(savedMeal);
-})
+  },
+);
 
-router.post(ROUTES.FOODS.FOODS_SAVE_QUICK_MEAL, async(req: TRequest<Pick<MealFull, 'quickMealId' | 'type'>>, res: TResponse<Meal | ErrorMiddlewareTypeProps>) => {
-    const meal = req.body;
+router.post(
+  ROUTES.FOODS.FOODS_SAVE_QUICK_MEAL,
+  async (
+    req: TRequest<
+      Pick<MealFull, 'quickMealId' | 'type'> & QuickMealQuantityFactor
+    >,
+    res: TResponse<Meal | ErrorMiddlewareTypeProps>,
+  ) => {
+    const {quantity_factor, ...meal} = req.body;
     const {uid} = res.locals.user!;
-    if(!meal || !meal.quickMealId) {
-        res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
-        return;
+    if (!meal || !meal.quickMealId) {
+      res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
+      return;
     }
-    const savedMeal = await saveQuickMealNutrition({...meal, uid, type: meal.type});
-    if(!savedMeal) {
-        res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
-        return;
+    const savedMeal = await saveQuickMealNutrition(
+      {
+        ...meal,
+        uid,
+        type: meal.type,
+      },
+      quantity_factor,
+    );
+    if (!savedMeal) {
+      res.status(404).json(ErrorMiddlewareTypes.NOTFOUND);
+      return;
     }
     res.status(200).json(savedMeal);
-})
-
+  },
+);
--- backend/src/routes/foods/types/product_apis.ts
+++ backend/src/routes/foods/types/product_apis.ts
@@ -1,15 +1,18 @@
-import { Product } from "../../../database/schema/product";
-
+import {Product} from '../../../database/schema/product';
 
 export type ProductQueryRequest = {
-    query: string;
-}
+  query: string;
+};
 
 export type ProductQRCodeRequest = {
-    qr_code: string;
-}
+  qr_code: string;
+};
 
 export type ProductQueryResponse = {
-    query: string;
-    products: Product[];
-}
+  query: string;
+  products: Product[];
+};
+
+export type QuickMealQuantityFactor = {
+  quantity_factor?: number; // [0-1] percentage (0-100% of total quantity)
+};
